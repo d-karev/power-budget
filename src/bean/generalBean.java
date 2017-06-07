89011cc4b0fc4b9a161f2e032ca3c3bd019de706
d-karev <dobromir.karev@gmail.com>
2017-06-07 19:40:06 +0300

    Internationalization
    - added internationalization
    - some fixes on the design

--- src/bean/generalBean.java
+++ src/bean/generalBean.java
@@ -5,18 +5,8 @@ import java.time.LocalDate;
 public class generalBean {
 
 	private final java.lang.Integer currentYear = LocalDate.now().getYear();
-	private final java.lang.String devTeamName = "Power Rangers Team";
-	private final java.lang.String productName = "Power Budget";
 	
 	public java.lang.Integer getCurrentYear() {
 		return currentYear;
-	}
-	
-	public java.lang.String getDevTeamName() {
-		return devTeamName;
-	}
-	
-	public java.lang.String getProductName() {
-		return productName;
 	}
 }
---
+++ src/bean/localeBean.java
@@ -0,0 +1,28 @@
+package bean;
+
+import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+
+public class localeBean {	
+    private Locale locale;
+
+    @PostConstruct
+    public void init() {
+        locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void setLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+}
